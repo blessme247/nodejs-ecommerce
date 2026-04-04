d8bcfd5ef8f4a8a0e168379f06253ea6caac638a
blessme247 <solabiabraham@gmail.com>
2026-04-04 17:12:13 +0100

    feat: exclude seller in load cart

--- middleware/loadCart.js
+++ middleware/loadCart.js
@@ -3,22 +3,23 @@ import Cart from "../model/Cart.js";
 export const loadCart = async (req, res, next) => {
    try {
       let cart;
+      const role = res.locals.user.role
+      if(role !== "Seller"){
 
-      // logged in user
-      console.log('trigger load cart')
-      if(req.userId){
-         console.log('check cart for user')
-         cart = await Cart.findOne({buyerId: req.userId}).exec()
+         if(req.userId){
+            console.log('check cart for user')
+            cart = await Cart.findOne({buyerId: req.userId}).exec()
+         }
+   
+         // guest with session
+         else if(req.sessionID) {
+            console.log('check cart for guest')
+            cart = await Cart.findOne({sessionId: req.sessionID}).exec()
+            // res.locals.user = {role: "Guest"}
+         }
+   
+         res.locals.cart = cart || { items: [], totalAmount: 0 };
       }
-
-      // guest with session
-      else if(req.sessionID) {
-         console.log('check cart for guest')
-         cart = await Cart.findOne({sessionId: req.sessionID}).exec()
-         // res.locals.user = {role: "Guest"}
-      }
-
-      res.locals.cart = cart || { items: [], totalAmount: 0 };
    } catch (error) {
        console.error('Error loading cart:', error);
     res.locals.cart = { items: [], totalAmount: 0 };
