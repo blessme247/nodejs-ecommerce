bb5c432bada6d5f9228bb9ab56d2c2b5676b30ba
blessme247 <solabiabraham@gmail.com>
2026-04-11 08:34:06 +0100

    fix(file structure): extract app handlers from mongo connection instance

--- server.js
+++ server.js
@@ -72,80 +72,80 @@ app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
 connectDB();
 
+const store = MongoStore.create({
+  client: mongoose.connection.getClient(),
+  collectionName: "sessions",
+  ttl: 7 * 24 * 60 * 60,
+  autoRemove: "native",
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store,
+    rolling: true, // Extend session on every request
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+  }),
+);
+
+const { csrfSynchronisedProtection } = csrfSync({
+  getTokenFromRequest: (req) => {
+    // If the incoming request is a application/x-www-form-urlencoded content type
+    // then get the token from the body.
+    if (req.is("application/x-www-form-urlencoded")) {
+      return req.body["_csrf"];
+    }
+    // Otherwise use the header for all other request types
+    return req.headers["x-csrf-token"];
+  },
+});
+
+app.use(csrfSynchronisedProtection);
+
+app.use((req, res, next) => {
+  //   console.log(req.session, 'session')
+  res.locals.csrfToken = req.csrfToken();
+  // Generate a unique nonce for each request
+  // res.locals.nonce = randomBytes(16).toString('base64');
+  next();
+});
+
+// app.use(mongoSanitize())
+
+app.use(useAuth);
+app.use(loadCart);
+
+app.use("/", indexRouter);
+
+app.use((req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not found" });
+  } else {
+    res.type("txt").send("404 Not found");
+  }
+});
+
+app.use(errorHandler);
+
 mongoose.connection.once("open", () => {
   console.log("Connected to database:", mongoose.connection.db.databaseName);
-
-  const store = MongoStore.create({
-    client: mongoose.connection.getClient(),
-    collectionName: "sessions",
-    ttl: 7 * 24 * 60 * 60,
-    autoRemove: "native",
-  });
-
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      store,
-      rolling: true, // Extend session on every request
-      cookie: {
-        secure: process.env.NODE_ENV === "production",
-        httpOnly: true,
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      },
-    }),
-  );
-
-  const { csrfSynchronisedProtection } = csrfSync({
-    getTokenFromRequest: (req) => {
-      // If the incoming request is a application/x-www-form-urlencoded content type
-      // then get the token from the body.
-      if (req.is("application/x-www-form-urlencoded")) {
-        return req.body["_csrf"];
-      }
-      // Otherwise use the header for all other request types
-      return req.headers["x-csrf-token"];
-    },
-  });
-
-  app.use(csrfSynchronisedProtection);
-
-  app.use((req, res, next) => {
-    //   console.log(req.session, 'session')
-    res.locals.csrfToken = req.csrfToken();
-    // Generate a unique nonce for each request
-    // res.locals.nonce = randomBytes(16).toString('base64');
-    next();
-  });
-
-  // app.use(mongoSanitize())
-
-  app.use(useAuth);
-  app.use(loadCart);
-
-  app.use("/", indexRouter);
-
-  app.use((req, res) => {
-    res.status(404);
-    if (req.accepts("html")) {
-      res.sendFile(path.join(__dirname, "views", "404.html"));
-    } else if (req.accepts("json")) {
-      res.json({ error: "404 Not found" });
-    } else {
-      res.type("txt").send("404 Not found");
-    }
-  });
-
-  app.use(errorHandler);
-
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
 });
 
 mongoose.connection.on("error", (err) => {
   console.error("MongoDB connection error:", err);
   process.exit(1);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
